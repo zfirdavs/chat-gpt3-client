f01c8edac48f502c55350e0aac223cd1fc5400ef
zfirdavs <firdavs792@gmail.com>
2023-02-23 20:02:10 +0500

    feat: add api key load from env file

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	godotenv.Load()
+
+	apiKey := os.Getenv("API_KEY")
+	if len(apiKey) == 0 {
+		log.Fatalln("API key is missing")
+	}
 
 }
